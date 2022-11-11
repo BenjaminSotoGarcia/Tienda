ace8dfcc4871e3bcedd2923eea5ae246159954bf
BenjaminSotoGarcia <benjamin.enriquesoto26@gmail.com>
2022-11-11 00:07:33 -0300

    final

---
+++ js/products.js
@@ -0,0 +1,66 @@
+const productos = [
+    {
+    id: 1,
+    nombre: "Becker",
+    precio: 500,
+    img:
+        "https://unimarc.vtexassets.com/arquivos/ids/213056-480-480/000000000000119376-UN-01.jpg?v=637532354220070000",
+    cantidad: 1,
+    },
+    {
+    id: 2,
+    nombre: "Escudo",
+    precio: 520,
+    img:
+        "https://r.btcdn.co/r/eyJzaG9wX2lkIjo0MDY0LCJnIjoiMTAwMHgifQ/f94f9d776de57d4/673657-7802100505477.jpg",
+    cantidad: 1,
+    },
+    {
+    id: 3,
+    nombre: "Heineken",
+    precio: 750,
+    img:
+        "https://www.elcielo.cl/tienda/635-large_default/cerveza-heineken-botella-330cc-5-.jpg",
+    cantidad: 1,
+    },
+    {
+    id: 4,
+    nombre: "Royal",
+    precio: 700,
+    img:
+        "https://eligetucerveza.cl/wp-content/uploads/2021/08/royal-guard-355cc-oferta-lager-1.jpg",
+    cantidad: 1,
+    },
+    {
+    id: 5,
+    nombre: "Corona",
+    precio: 650,
+    img:
+        "https://redtaurus.cl/tienda/wp-content/uploads/2021/03/corona710-600x600.jpg",
+    cantidad: 1,
+    },
+    {
+    id: 6,
+    nombre: "Cusqueña",
+    precio: 850,
+    img:
+        "https://www.lacavadepedro.cl/wp-content/uploads/2021/07/goldlagercuzquen%CC%83a.png",
+    cantidad: 1,
+    },
+    {
+    id: 7,
+    nombre: "Kunstmann",
+    precio: 1000,
+    img:
+        "https://www.elcielo.cl/tienda/1088-medium_default/cerveza-kunstmann-torobayo-botella-500cc-5.jpg",
+    cantidad: 1,
+    },
+    {
+    id: 8,
+    nombre: "Budwaiser",
+    precio: 800,
+    img:
+        "https://cdnx.jumpseller.com/comercial-jp/image/10938505/budweiser_355cc_botella.webp?1627017715",
+    cantidad: 1,
+    },
+];
